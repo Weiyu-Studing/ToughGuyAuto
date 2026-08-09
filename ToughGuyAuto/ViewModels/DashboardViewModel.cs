3d62a5b0fe5498117c5741ac29f16c6df95a49e6
weiyu <yin1532776643@gmail.com>
2026-08-09 16:24:42 -0500

    complete view models

--- ToughGuyAuto/ViewModels/DashboardViewModel.cs
+++ ToughGuyAuto/ViewModels/DashboardViewModel.cs
@@ -1,6 +1,27 @@
-﻿namespace ToughGuyAuto.ViewModels
+﻿namespace ToughGuyAuto.ViewModels;
+
+public class DashboardViewModel
 {
-    public class DashboardViewModel
-    {
-    }
+    public int TotalVehicles { get; set; }
+
+    public int TotalMaintenanceRecords { get; set; }
+
+    public decimal TotalMaintenanceCost { get; set; }
+
+    public string? MostUsedServiceType { get; set; }
+
+    public List<RecentMaintenanceViewModel> RecentMaintenance
+    { get; set; } = new();
+}
+
+public class RecentMaintenanceViewModel
+{
+    public string VehicleName { get; set; } = string.Empty;
+
+    public DateTime ServiceDate { get; set; }
+
+    public decimal Cost { get; set; }
+
+    public List<string> ServiceTypes { get; set; }
+        = new();
 }
--- ToughGuyAuto/ViewModels/MaintenanceRecordEditViewModel.cs
+++ ToughGuyAuto/ViewModels/MaintenanceRecordEditViewModel.cs
@@ -1,6 +1,33 @@
-﻿namespace ToughGuyAuto.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToughGuyAuto.ViewModels;
+
+public class MaintenanceRecordEditViewModel
 {
-    public class MaintenanceRecordEditViewModel
-    {
-    }
+    public int MaintenanceRecordId { get; set; }
+
+    [Required]
+    public int VehicleId { get; set; }
+
+    [Required]
+    [DataType(DataType.Date)]
+    public DateTime ServiceDate { get; set; }
+
+    [Required]
+    [Range(0, int.MaxValue)]
+    public int Mileage { get; set; }
+
+    [Required]
+    [Range(0, double.MaxValue)]
+    public decimal Cost { get; set; }
+
+    [Required]
+    [StringLength(500)]
+    public string Description { get; set; } = string.Empty;
+
+    [StringLength(1000)]
+    public string Notes { get; set; } = string.Empty;
+
+    public List<int> SelectedServiceTypeIds { get; set; }
+        = new List<int>();
 }
